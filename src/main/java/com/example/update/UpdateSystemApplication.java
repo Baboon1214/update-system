c17482834b1a7d811a8c0dc83a4d6114e427ab4f
Baboon1214 <baboonbirbek@gmail.com>
2026-05-01 17:10:58 +0300

    init spring boot project structure

---
+++ src/main/java/com/example/update/UpdateSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateSystemApplication.class, args);
+	}
+
+}
